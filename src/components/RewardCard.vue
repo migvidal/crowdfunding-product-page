47e75384d7e0c448959d1f3021678c7490025721
Miguel Vidal <68684733+migvidal@users.noreply.github.com>
2021-12-10 14:02:29 +0100

    responsive reward cards

--- src/components/RewardCard.vue
+++ src/components/RewardCard.vue
@@ -3,15 +3,17 @@
     <h2>{{ reward.name || "Pledge with no reward" }}</h2>
     <p v-if="reward.minPledge">Pledge ${{ reward.minPledge }} or more</p>
     <p>{{ reward.description }}</p>
-    <p v-if="reward.units">
-      <big-text>{{ reward.units }} </big-text> left
-    </p>
-    <cf-button
-      @click="$emit('btnclick')"
-      :disabled="reward.id !== 0 && !reward.units"
-    >
-      {{ reward.id === 0 || reward.units ? "Select Reward" : "Out of Stock" }}
-    </cf-button>
+    <div class="flex-container">
+      <p v-if="reward.units">
+        <big-text>{{ reward.units }} </big-text> left
+      </p>
+      <cf-button
+        @click="$emit('btnclick')"
+        :disabled="reward.id !== 0 && !reward.units"
+      >
+        {{ reward.id === 0 || reward.units ? "Select Reward" : "Out of Stock" }}
+      </cf-button>
+    </div>
   </div>
 </template>
 
@@ -33,5 +35,10 @@ export default {
 .rounded-card.reward {
   padding: 1rem;
   margin: 1rem 0;
+}
+.flex-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
